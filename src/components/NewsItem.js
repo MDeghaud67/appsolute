cc9d80c9b459840c1f024d3127f93a4337dd5af7
MDeghaud67 <michel.deghaud@gmail.com>
2023-02-16 16:44:33 +0100

    article list card

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -21,47 +21,47 @@ function NewsItem() {
     .catch((error) => {
         setError(error);
     })*/
-}, [])
+}, [url])
 const listArticle = news.articles
 //console.log(listArticle)
 
-/*const card = Array.isArray(listArticle) ? listArticle.map((article) => {
+const card = Array.isArray(listArticle) ? listArticle.map((article) => {
+  return (
     <Card style={{ width: '18rem' }}>
-    <Card.Img variant="top" src="holder.js/100px180" />
-    <Card.Body>
-      <Card.Title>{ article.title }</Card.Title>
-      <Card.Text>
-        { article.source.name }
-      </Card.Text>
-    </Card.Body>
-  </Card>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>{article.title}</Card.Title>
+        <Card.Text>
+          {article.source.name}
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  )
   
   //,console.log(news.articles.source) 
-}) :[] */
+}) :[] 
 
-const title = Array.isArray(listArticle) ? listArticle.map((article, index) => {
+/*const title = Array.isArray(listArticle) ? listArticle.map((article, index) => {
   // eslint-disable-next-line no-unused-expressions
   return (
-    article.title
+    <Card.Title key={index}>{article.title}</Card.Title>
   )
 }) : []
-const source = Array.isArray(listArticle) ? listArticle.map((article) => {
-  return(article.source.name)
-}) : []
+const source = Array.isArray(listArticle) ? listArticle.map((article, index) => {
+  return( 
+    <Card.Text key={index}>
+      {article.source.name}
+    </Card.Text>
+  )
+}) : []*/
   // for (const article in listArticle) {
       //console.log(listArticle.source.name)
       // console.log(article)
     // }
     return (
-      <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>{ title }</Card.Title>
-        <Card.Text>
-          { source }
-        </Card.Text>
-      </Card.Body>
-    </Card>
+      <>
+        {card}
+      </>
     
     //,console.log(news.articles.source)
     )   
