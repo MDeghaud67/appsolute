551991d126b5fd469e95805ddbc87c78c919f324
MDeghaud67 <michel.deghaud@gmail.com>
2023-02-21 14:44:56 +0100

    search

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import Banner from "../components/Banner";
 import NewsList from "../components/NewsList";
 
 function Home(){
+    const [options, setOptions] = useState([]);
+    const [loading, setLoading] = useState(false);
     return (
         <><Banner /><NewsList /></>
     )
